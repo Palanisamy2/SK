aab058cae0beffce46d61d5cda3fab3d586de2a5
Palanisamy M <146609846+Palanisamy2@users.noreply.github.com>
2024-11-05 12:57:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <ShopContextProvider>
-        <Router>
+        <Router basename={'resume-site'}>
           <Navbar />
           <Routes>
             <Route path="/" element={<Shop />} />
